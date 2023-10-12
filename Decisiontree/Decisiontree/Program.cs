3fea9df87f22cd549b4a7989a2b0b4a7e601e5c5
n0wraid <147038074+n0wraid@users.noreply.github.com>
2023-10-12 21:12:12 +0600

    Add files via upload

---
+++ Decisiontree/Decisiontree/Program.cs
@@ -0,0 +1,41 @@
+﻿using Accord.MachineLearning;
+using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.MachineLearning.VectorMachines.Markov;
+using Accord.MachineLearning.VectorMachines.Markov.Learning;
+using Accord.Math;
+using Accord.Math.Optimization.Losses;
+using Accord.Statistics.Kernels;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Sample data
+        double[][] inputs =
+        {
+            new double[] { 0, 0 },
+            new double[] { 1, 0 },
+            new double[] { 0, 1 },
+            new double[] { 1, 1 }
+        };
+
+        int[] outputs = { -1, 1, 1, -1 }; // Example binary classification labels
+
+        // Create a Support Vector Machine with a polynomial kernel
+        SupportVectorMachine<Gaussian> svm = new SupportVectorMachine<Gaussian>(inputs: 2);
+
+        // Create a Sequential Minimal Optimization (SMO) learning algorithm
+        SequentialMinimalOptimization<Gaussian> smo = new SequentialMinimalOptimization<Gaussian>(svm, inputs, outputs);
+
+        // Train the decision tree
+        double error = smo.Run(); // Training
+
+        // Make predictions
+        int prediction1 = System.Math.Sign(svm.Decide(new double[] { 0, 0 }));
+        int prediction2 = System.Math.Sign(svm.Decide(new double[] { 1, 1 }));
+
+        Console.WriteLine($"Prediction 1: {prediction1}");
+        Console.WriteLine($"Prediction 2: {prediction2}");
+    }
+}
---
+++ Randontree/Randontree/Program.cs
@@ -0,0 +1,44 @@
+﻿using Accord.MachineLearning;
+using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.MachineLearning.VectorMachines.Learning.StochasticGradientDescent;
+using Accord.MachineLearning.VectorMachines.Markov;
+using Accord.MachineLearning.VectorMachines.Markov.Learning;
+using Accord.Statistics.Kernels;
+using Accord.MachineLearning.VectorMachines.Learning.SMO;
+using Accord.MachineLearning.VectorMachines.SupportVector;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Sample data
+        double[][] inputs =
+        {
+            new double[] { 0, 0 },
+            new double[] { 1, 0 },
+            new double[] { 0, 1 },
+            new double[] { 1, 1 }
+        };
+
+        int[] outputs = { -1, 1, 1, -1 }; // Example binary classification labels
+
+        // Create a Random Forest classifier with decision trees
+        var forest = new RandomForest()
+        {
+            NumberOfTrees = 100, // Number of trees in the forest
+            MaximumVariables = 2, // Maximum number of variables to consider at each split
+            DecisionTree = new C45Learning()
+        };
+
+        // Train the random forest
+        var randomForest = forest.Learn(inputs, outputs);
+
+        // Make predictions
+        int prediction1 = randomForest.Decide(new double[] { 0, 0 });
+        int prediction2 = randomForest.Decide(new double[] { 1, 1 });
+
+        Console.WriteLine($"Prediction 1: {prediction1}");
+        Console.WriteLine($"Prediction 2: {prediction2}");
+    }
+}
